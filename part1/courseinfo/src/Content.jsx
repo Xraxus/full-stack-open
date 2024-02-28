4eba6481de67b712c4284de1f1ad5ca69da1c348
Kamil Kobylarz <xraxus@gmail.com>
2024-02-28 19:49:36 +0100

    Complete exercise 1.2

--- part1/courseinfo/src/Content.jsx
+++ part1/courseinfo/src/Content.jsx
@@ -1,15 +1,11 @@
+import Part from "./Part";
+
 function Content({ part1, part2, part3 }) {
   return (
     <>
-      <p>
-        {part1.name} - {part1.exercises}
-      </p>
-      <p>
-        {part2.name} - {part2.exercises}
-      </p>
-      <p>
-        {part3.name} - {part3.exercises}
-      </p>
+      <Part name={part1.name} exercises={part1.exercises} />
+      <Part name={part2.name} exercises={part2.exercises} />
+      <Part name={part3.name} exercises={part3.exercises} />
     </>
   );
 }
