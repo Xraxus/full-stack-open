588308b2049480c33c39ecdb36d270de04508b48
Kamil Kobylarz <xraxus@gmail.com>
2024-02-28 19:46:18 +0100

    Complete exercise 1.1

---
+++ part1/courseinfo/src/Content.jsx
@@ -0,0 +1,17 @@
+function Content({ part1, part2, part3 }) {
+  return (
+    <>
+      <p>
+        {part1.name} - {part1.exercises}
+      </p>
+      <p>
+        {part2.name} - {part2.exercises}
+      </p>
+      <p>
+        {part3.name} - {part3.exercises}
+      </p>
+    </>
+  );
+}
+
+export default Content;
---
+++ part1/courseinfo/src/Total.jsx
@@ -0,0 +1,5 @@
+function Total({number}) {
+  return <p>Total number of exercises: {number}</p>;
+}
+
+export default Total;
