92cc82f09927197637a4c63966e55d44ab4cae05
Kamil Kobylarz <xraxus@gmail.com>
2024-02-29 13:32:59 +0100

    Complete exercise 1.4

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -4,29 +4,26 @@ import Total from "./Total";
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10,
-  };
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7,
-  };
-  const part3 = {
-    name: "State of a component",
-    exercises: 14,
-  };
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
 
   return (
     <div>
       <Header course={course} />
-      <Content
-        part1={{ name: part1.name, exercises: part1.exercises }}
-        part2={{ name: part2.name, exercises: part2.exercises }}
-        part3={{ name: part3.name, exercises: part3.exercises }}
-      />
-      <hr />
-      <Total number={part1.exercises + part2.exercises + part3.exercises} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   );
 };
--- part1/courseinfo/src/Total.jsx
+++ part1/courseinfo/src/Total.jsx
@@ -1,5 +1,10 @@
-function Total({number}) {
-  return <p>Total number of exercises: {number}</p>;
+function Total({ parts }) {
+  return (
+    <p>
+      Total number of exercises:{" "}
+      {parts.reduce((acc, item) => acc + item.exercises, 0)}
+    </p>
+  );
 }
 
 export default Total;
