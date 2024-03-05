544058af760bf1a85cd99704e419afbbe5ae588a
Kamil Kobylarz <xraxus@gmail.com>
2024-03-05 14:11:22 +0100

    complete 2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,26 +1,43 @@
 import { useState } from "react";
 
 function App() {
-  const [contacts, setContacts] = useState([{ name: "Arto Hellas" }]);
+  const [contacts, setContacts] = useState([
+    { name: "Arto Hellas", tel: 123456789 },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newTel, setNewTel] = useState("");
 
   function handleSubmit(e) {
     e.preventDefault();
     if (contacts.some((contact) => contact.name === newName))
       alert(`${newName} is already in the phonebook`);
-    else setContacts([...contacts, { name: newName }]);
+    else setContacts([...contacts, { name: newName, tel: newTel }]);
     setNewName("");
+    setNewTel("");
   }
 
-  function handleChange(e) {
-    setNewName(e.target.value);
-  }
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} className="flex-column">
         <label>
-          name: <input type="text" value={newName} onChange={handleChange} />
+          name:{" "}
+          <input
+            type="text"
+            value={newName}
+            onChange={(e) => setNewName(e.target.value)}
+            placeholder="Leia Skywalker"
+          />
+        </label>
+        <label>
+          number:{" "}
+          <input
+            type="tel"
+            pattern="[0-9]{9}"
+            placeholder="123456789"
+            value={newTel}
+            onChange={(e) => setNewTel(e.target.value)}
+          />
         </label>
         <div>
           <button type="submit">add</button>
@@ -29,7 +46,9 @@ function App() {
       <h2>Numbers</h2>
       <ul>
         {contacts.map((contact) => (
-          <li key={contact.name}>{contact.name}</li>
+          <li key={contact.name}>
+            {contact.name} - {contact.tel}
+          </li>
         ))}
       </ul>
     </div>
