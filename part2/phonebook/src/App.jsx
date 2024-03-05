881cdf2540f17887fbf7412c6b01bca419e965b2
Kamil Kobylarz <xraxus@gmail.com>
2024-03-05 14:24:11 +0100

    complete 2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,9 +3,19 @@ import { useState } from "react";
 function App() {
   const [contacts, setContacts] = useState([
     { name: "Arto Hellas", tel: 123456789 },
+    { name: "Ada Lovelace", tel: 394453235 },
+    { name: "Dan Abramov", tel: 12432343 },
+    { name: "Mary Poppendieck", tel: 392364231 },
   ]);
   const [newName, setNewName] = useState("");
   const [newTel, setNewTel] = useState("");
+  const [filterName, setFilterName] = useState("");
+
+  const filteredConcats = filterName
+    ? contacts.filter((contact) =>
+        contact.name.toLowerCase().includes(filterName.toLowerCase())
+      )
+    : contacts;
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -18,7 +28,16 @@ function App() {
 
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
+      <label>
+        filter by name:{" "}
+        <input
+          type="text"
+          value={filterName}
+          onChange={(e) => setFilterName(e.target.value)}
+        />
+      </label>
+      <h2>Add new contact</h2>
       <form onSubmit={handleSubmit} className="flex-column">
         <label>
           name:{" "}
@@ -45,7 +64,7 @@ function App() {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {contacts.map((contact) => (
+        {filteredConcats.map((contact) => (
           <li key={contact.name}>
             {contact.name} - {contact.tel}
           </li>
